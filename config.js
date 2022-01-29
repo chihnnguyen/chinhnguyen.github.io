40a9115c4f4b0f05bfeec4b925b5214577c68285
chihnnguyen <98662069+chihnnguyen@users.noreply.github.com>
2022-01-30 02:33:30 +0700

    ,

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào công chúa ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, nhưng em hứa phải trả lời thật lòng nha`,
+    btnIntro: 'anh iu hỏi điii',
+    title: 'Anh biết thém thích Anh phải không phải không nè ? 😙',
+    desc: 'thém mà không trả lời hoặc thoát ra, tức là thém thích Anh đó nha :> ',
+    btnYes: 'Cóooo, thậm chí là yêu luôn cơ 💖💖💖',
+    btnNo: '💔 Anh nằm mơ à ! 💔 ',
+    question:'kể cho anh nghe đi thém thích anh ở điểm gì nè 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó nên em mới yêu anh Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng yêu thém lắm 🥰. Yêu thém mãi mãi 😘😘',
+    messDesc: '💬💬 Thém đồng ý rồi thì làm người yêu anh nha. 💬💬',
+    btnAccept: 'em đồng ý. Yêu anh 💖',
+    messLink: 'https://www.facebook.com/nguyenchinh03'
+}
